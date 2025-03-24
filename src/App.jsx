1f482474fff985738758cb91d7b17768083e325c
Jhonatan <jhowbrows321@gmail.com>
2025-03-24 17:25:24 -0300

    Configurando o erro e visual do input

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Perfil from "./components/Perfil";
 import Formulario from "./components/Formulario";
 import ReposList from "./components/ReposList";
 
+
 function App() {
   const [formularioEstaVisivel, setFormularioEstaVisivel] = useState(true);
   const [nomeUsuario, setNomeUsuario] = useState('');
@@ -11,14 +12,16 @@ function App() {
 
   return (
     <>
-      <input type="text" onBlur={(e) => setNomeUsuario(e.target.value)} />
+      <div className="contentCenter">
+        <input className="inputStyle" type="text" onBlur={(e) => setNomeUsuario(e.target.value)} placeholder="Digíte o nome de usúario Github" />
 
-      {nomeUsuario.length > 4 && (
-        <>
-          <Perfil nomeUsurario={nomeUsuario} />
-          <ReposList nomeUsuario={nomeUsuario}/>
-        </>
-      )}
+        {nomeUsuario.length > 4 && (
+          <>
+            <Perfil nomeUsurario={nomeUsuario} />
+            <ReposList nomeUsuario={nomeUsuario}/>
+          </>
+        )}
+      </div>
 
     
       {/* {formularioEstaVisivel && (
--- src/components/ReposList/index.jsx
+++ src/components/ReposList/index.jsx
@@ -6,26 +6,39 @@ import styles from './ReposList.module.css';
 const ReposList = ({ nomeUsuario }) => {
     const [repos, setRepos] = useState([]);
     const [estaCarregando, setEstaCarregando] = useState(true);
+    const [erro, setErro] = useState(false);
 
 
     useEffect(() => {
         setEstaCarregando(true);
         fetch(`https://api.github.com/users/${nomeUsuario}/repos`)
-        .then(res => res.json())
-        .then(resJson => {
+        .then((res) => {
+            if (!res.ok) {
+              throw new Error("Usuário não encontrado");
+            }
+            return res.json();
+        })
+        .then((resJson) => {
             setTimeout(() => {
                 setEstaCarregando(false);
                 setRepos(resJson);
 
-            }, 3000)
+            }, 2000)
         })
+        .catch((e) => {
+            setEstaCarregando(false);
+            setErro(true);
+            console.error("Erro no carregamento do repositório:",e.message);
+        });
     }, [nomeUsuario])
 
     return (
         <div className="container">
             {estaCarregando ? (
                 <h1>Carregando...</h1>
-            ): (
+            ): erro ? (
+                <p className={styles.erroMsg}>Falha ao carregar repositório. Verifique o nome de usúario digitado.</p>
+            ) : (
                 <ul className={styles.list}>
                     {/* {repos.map(repositorio => ( */}
                     {repos.map(({id, name, language, html_url}) => ( 
